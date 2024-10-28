de3b111f096176d37ac44884ec74c6bc516e3553
Igor Froehner <igoor.sf14@gmail.com>
2024-10-28 12:02:45 -0300

    tests: specific context to the first failure

--- spec/sidekiq/silent_retry/server_middleware_spec.rb
+++ spec/sidekiq/silent_retry/server_middleware_spec.rb
@@ -10,12 +10,36 @@ RSpec.describe Sidekiq::SilentRetry::ServerMiddleware do
     -> { raise error_class, "some message" }
   end
   let(:job_payload) do
-    payload = {
+    {
+      "retry_count" => retry_count,
       "retry" => 2,
       "silent_retry" => silent_retry
     }
-    payload["retry_count"] = retry_count if retry_count
-    payload
+  end
+
+  context 'in the first failure' do
+    let(:job_payload) do
+      {
+        "retry" => 2,
+        "silent_retry" => silent_retry
+      }
+    end
+
+    context 'with silent retry enabled' do
+      let(:silent_retry) { true }
+
+      it 'raises the silent error' do
+        expect { subject }.to raise_error(Sidekiq::SilentRetry.silent_retry_error_class, "some message")
+      end
+    end
+
+    context 'with silent retry disabled' do
+      let(:silent_retry) { false }
+
+      it 'raises the error' do
+        expect { subject }.to raise_error(StandardError, "some message")
+      end
+    end
   end
 
   context "when silent retry is off" do
